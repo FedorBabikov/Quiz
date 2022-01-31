1b2f4c9e93c57a1f558b165012b62bf0946e2b4c
Fedor Babikov <fed.babikov@protonmail.com>
2022-01-31 13:30:37 +0100

    added error message upon multiple clicks on options

--- src/pages/questionPage.js
+++ src/pages/questionPage.js
@@ -37,8 +37,9 @@ export const initQuestionPage = (userInterface, refresh = '') => {
   }
 
   const currentQuestion = quizData.questions[quizData.currentQuestionIndex++];
-  let click = false;
-  let clickCount = 0;
+  let optionClicked = false;
+  let clickCountButton = 0;
+  let clickCountOption = 0;
 
   const questionElement = createQuestionElement(
     currentQuestion.text,
@@ -59,49 +60,74 @@ export const initQuestionPage = (userInterface, refresh = '') => {
     option.addEventListener('click', chooseAnswer);
   }
 
+  setTimer();
+
+  const reference = document.getElementById(REFERENCE_LIST_ID);
+
+  for (const link of currentQuestion.links) {
+    const referenceElement = createReferenceElement(link.text, link.href);
+    reference.appendChild(referenceElement);
+  };
+
+  document
+    .getElementById(
+      isLastQuestion() ? GET_RESULT_BUTTON_ID : NEXT_QUESTION_BUTTON_ID
+    )
+    .addEventListener('click', function () {
+      nextStep(optionClicked);
+    });
+
+  // Service functions
+
   function chooseAnswer(e) {
-    click = true;
-    currentQuestion.selected = e.target.dataset.key;
+    optionClicked = true;
+    clickCountOption++
     const timer = document.getElementById(TIMER_ELEMENT_ID);
-    const remainedTime = timer.innerHTML.slice(3);
-    if (currentQuestion.selected !== currentQuestion.correct) {
-      e.target.classList.add('wrong-select');
-      quizData.wrongSum++;
-    } else {
-      e.target.classList.add('correct-select');
-      quizData.correctSum++;
-    }
-    if (currentQuestion.selected === currentQuestion.correct) {
-      if (remainedTime > 30) {
-        quizData.timeScore++;
-      }
-    }
-    const correctAnswer = document.querySelector(
-      `li[data-key="${currentQuestion.correct}"]`
-    );
-    correctAnswer.style.backgroundColor = 'green';
-
-    for (const option of answersListElement.children) {
-      option.removeEventListener('click', chooseAnswer);
-      option.style.color = "black";
-    }
-
+    const timeLeft = timer.innerHTML.slice(3);
     const clickError = document.getElementById('error');
-    if (!!clickError) {
-      clickError.remove();
+
+    if (clickCountOption <= 1) {
+      currentQuestion.selected = e.target.dataset.key;
+      
+      if (currentQuestion.selected !== currentQuestion.correct) {
+        e.target.classList.add('wrong-select');
+        quizData.wrongSum++;
+      } else {
+        e.target.classList.add('correct-select');
+        quizData.correctSum++;
+      }
+      if (currentQuestion.selected === currentQuestion.correct) {
+        if (timeLeft > 30) {
+          quizData.timeScore++;
+        }
+      }
+      const correctAnswer = document.querySelector(
+        `li[data-key="${currentQuestion.correct}"]`
+      );
+      correctAnswer.style.backgroundColor = 'green';
+  
+      for (const option of answersListElement.children) {
+        option.style.color = "black";
+      }
+  
+      if (!!clickError) {
+        clickError.remove();
+      }
+
+    } else {
+        if (!!clickError) {
+          clickError.innerHTML = 'You have already made your choice. One time is enough!';
+        } else {
+          showError('option');
+        }
     }
   }
 
-  setTimer();
-
-  function nextStep(click) {
-    clickCount++;
-    if (!click) {
-      if (clickCount < 2) {
-        const clickError = createErrorElement(isLastQuestion());
-        clickError.classList.add('error');
-        clickError.setAttribute('id', 'error');
-        answersListElement.appendChild(clickError);
+  function nextStep(optionClicked) {
+    clickCountButton++;
+    if (!optionClicked) {
+      if (clickCountButton < 2) {
+        showError('button');
       }
     } else {
       sessionStorage.removeItem(`question${quizData.currentQuestionIndex - 1}`);
@@ -109,24 +135,17 @@ export const initQuestionPage = (userInterface, refresh = '') => {
     }
   }
 
-  const reference = document.getElementById(REFERENCE_LIST_ID);
+  function showError(clickedNode) {
+    const clickError = createErrorElement(isLastQuestion(), clickedNode);
+    clickError.classList.add('error');
+    clickError.setAttribute('id', 'error');
+    answersListElement.appendChild(clickError);
+  }
 
-  currentQuestion.links.forEach((link) => {
-    const referenceElement = createReferenceElement(link.text, link.href);
-    reference.appendChild(referenceElement);
-  });
-
-  document
-    .getElementById(
-      isLastQuestion() ? GET_RESULT_BUTTON_ID : NEXT_QUESTION_BUTTON_ID
-    )
-    .addEventListener('click', function () {
-      nextStep(click);
-    });
-};
-
-function isLastQuestion() {
+  function isLastQuestion() {
   return quizData.currentQuestionIndex < quizData.questions.length
     ? false
     : true;
-}
+  }
+
+};
--- src/views/clickErrorView.js
+++ src/views/clickErrorView.js
@@ -1,14 +1,20 @@
 'use strict';
 
-export const createErrorElement = (isLast) => {
+export const createErrorElement = (isLast, clickedNode) => {
   
   const element = document.createElement('p');
 
-  element.innerHTML = String.raw`
-    To see ${
+  switch (clickedNode) {
+    case 'option' :
+      element.innerHTML = 'You have already made your choice. One time is enough!';
+      break;
+    case 'button' :
+      element.innerHTML = String.raw`
+      To see ${
       isLast ? 'the result' : 'the next Q'
-    }, please first select an answer for this ${isLast ? 'last Q' : 'one'}.
-    `;
+      }, please first select an answer for this ${isLast ? 'last Q' : 'one'}.`;
+      break;
+  }
 
   return element;
 };
